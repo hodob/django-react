8c388973b3b808044fb1c12ba0f2a7e0143193c7
지정호 <hodob76@gmail.com>
2023-05-18 17:04:13 +0900

    back cange

--- backend/backendproject/api/views.py
+++ backend/backendproject/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+
+def main(request):
+    message = request.GET.get('abc')
+    print(message)
+
+    return HttpResponse("안녕?")
