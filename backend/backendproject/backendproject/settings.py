56b5d110d828e9da868862ac05c9c291f182b17c
지정호 <hodob76@gmail.com>
2023-05-18 16:43:22 +0900

    installapp change

--- backend/backendproject/backendproject/settings.py
+++ backend/backendproject/backendproject/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/ddd
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-2!7f8p5g$ibov58ko+wl&wi4b90jjjs%y0z5v(2ig41_a#8rxi"
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'app',
+    'backendapp',
     'rest_framework',
 ]
 
