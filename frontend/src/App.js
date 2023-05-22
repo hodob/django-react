772f706355d40c8fade3a17bb0d0c331e5f4e34b
지정호 <hodob76@gmail.com>
2023-05-22 16:02:31 +0900

    cors 건들기전

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,12 @@ import React, { useState } from "react";
 import axios from "axios";
 
 function App() {
+
+
+  axios.defaults.xsrfCookieName = 'csrftoken';
+  axios.defaults.xsrfHeaderName = 'X-CSRFToken';
   const [text, setText] = useState("없음");
-  
+
   const clicked = () => {
     axios
       .get("http://127.0.0.1:8000", {
@@ -16,30 +20,13 @@ function App() {
       // .then((response) => console.log(response));
       .then((response) => setText(JSON.stringify(response.data)));
   };
-  const test = (<div>test</div>);
+
 
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-        <div>
+
+    <div>
       <h1>{text}</h1>
       <button onClick={clicked}>클릭</button>
-      <button onClick={test}>클릭2</button>
-      {test}
     </div>
   );
 }
