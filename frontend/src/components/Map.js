64d6dea7c4b4f9b1fdbf56964fe89fdfde0daeb4
지정호 <hodob76@gmail.com>
2023-05-25 10:17:14 +0900

    지도 표시 완

---
+++ frontend/src/components/Map.js
@@ -0,0 +1,10 @@
+import KakaoMap from "./KakaoMap";
+
+export default function Map() {
+return (
+    <>
+    <h1>지도</h1>
+    <KakaoMap />
+    </>
+);
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/App';
+import Gallery from './components/Gallery';
+import Map from './components/Map';
 import reportWebVitals from './reportWebVitals';
-import axios from 'axios';
+import {  BrowserRouter,  Routes,  Route,} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+  <BrowserRouter>
+  <Routes>
+      <Route exact path="/" element={<App/>}></Route>
+      <Route path="/gallery" element={<Gallery/>}></Route>
+      <Route path="/map" element={<Map/>}></Route>
+
+  </Routes>
+</BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
