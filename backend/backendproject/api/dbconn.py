73d683d0374125e4b283e2cbef660d5d58248fbe
지정호 <hodob76@gmail.com>
2023-05-26 16:13:10 +0900

    인자 적용 확인

---
+++ backend/backendproject/api/dbconn.py
@@ -0,0 +1,14 @@
+# import psycopg
+# conn = psycopg.connect(host='jjjteam.duckdns.org',dbname='tp_db',user='postgres',password='wjdgh7578@',port=5432)
+# cur=conn.cursor()
+# cur.execute("SELECT * FROM public.inu_obs_mi ORDER BY pk_id ASC LIMIT 100")
+
+# dbURL = 'jjjteam.duckdns.org'
+# db="tp_db"
+# dbPort = 5432
+# dbUser = "postgres"
+# dbPass = "wjdgh7578@"
+
+# def cursor2(): # out curser()
+#     return psycopg.connect(host='jjjteam.duckdns.org',dbname='tp_db',user='postgres',password='wjdgh7578@',port=5432).cursor()
+
--- backend/backendproject/api/urls.py
+++ backend/backendproject/api/urls.py
@@ -4,6 +4,6 @@ from .views import *
 
 urlpatterns = [
     path('api/', inu_obs_mi_data.as_view()),
-    # path('api2/', main5.as_view()),
+    path('api2/', api_test.as_view()),
     # path('api3/', main6.as_view()), # 다른테이블 가져오기
 ]
