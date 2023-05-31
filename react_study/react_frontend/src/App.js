0936c3f1b4b77519f67cb2a8267c860e51897a40
지정호 <hodob76@gmail.com>
2023-05-31 16:15:11 +0900

    버튼클릭

--- react_study/react_frontend/src/App.js
+++ react_study/react_frontend/src/App.js
@@ -1,43 +1,18 @@
-import './App.css';
+import "./App.css";
+import { useState } from 'react';
 
 
 
-function MyButton() {
-  return(
-    <button>Click me!</button>
-  )
-}
 
-const user = {
-  name: 'John',
-  imageUrl: 'https://i.pravatar.cc/300',
-  imageSize: 100,
-};
-const products = [
-  { title: 'Cabbage', isFruit: false, id: 1 },
-  { title: 'Garlic', isFruit: false, id: 2 },
-  { title: 'Apple', isFruit: true, id: 3 },
-];
 
 function App() {
-  const listItems = products.map((product) => (
-    <li 
-      key={product.id}
-      style={{ color: product.isFruit ? 'red' : 'green' }}
-    >
-    {product.title}
-      </li>
-  ));
+  function handleClick() {
+    alert("Button clicked");
+  }
 
   return (
     <>
-      {/* <h1>{user.name}</h1>
-      <img src={user.imageUrl} alt={user.name} width={user.imageSize} />
-      <MyButton /> */}
-      <ul>
-        <h1> test</h1>
-        {listItems}
-      </ul>
+      <button onClick={handleClick}>Click me</button>
     </>
   );
 }
