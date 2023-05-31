65fdecc6a1d031c1ba921e73808bce37282d1db3
지정호 <hodob76@gmail.com>
2023-05-31 15:59:52 +0900

    study

--- react_study/react_frontend/src/App.js
+++ react_study/react_frontend/src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
 
 
+
+function MyButton() {
+  return(
+    <button>Click me!</button>
+  )
+}
+
+const user = {
+  name: 'John',
+  imageUrl: 'https://i.pravatar.cc/300',
+  imageSize: 100,
+};
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
+
 function App() {
   return (
     <div>
-      <h1>React App</h1>
+      <h1>{user.name}</h1>
+      <img src={user.imageUrl} alt={user.name} width={user.imageSize} />
+      <MyButton />
     </div>
   );
 }
-
 export default App;
