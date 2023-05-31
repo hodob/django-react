fe1ff3432f2bcef06772725c1b47c53fa0cc16d4
지정호 <hodob76@gmail.com>
2023-05-31 17:29:02 +0900

    Props.defaultProps

--- react_study/react_frontend/src/components/props.js
+++ react_study/react_frontend/src/components/props.js
@@ -6,6 +6,11 @@ export default function Props(props, {age}) {
             <h1>Props</h1>
             <h2>{props.name}</h2>
             <h2>{age}</h2>
+            <h2>{props.name2}</h2>
         </div>
     )
+    
 }
+Props.defaultProps = {
+    name2 : '추가된이름'
+};
