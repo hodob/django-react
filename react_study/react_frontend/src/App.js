a5b4f6d0a74f622a776cf91ad24cf031d263f30d
지정호 <hodob76@gmail.com>
2023-06-01 14:07:42 +0900

    useCallback 을 사용하여 함수 재사용하기

--- react_study/react_frontend/src/App.js
+++ react_study/react_frontend/src/App.js
@@ -1,5 +1,5 @@
 
-import React, { useRef, useState, useMemo } from "react";
+import React, { useRef, useState, useMemo, useCallback } from "react";
 import UserList from "./components/UserList";
 import CreateUser from "./components/CreateUser";
 
@@ -14,13 +14,23 @@ function App() {
     email: ''
   });
   const { username, email } = inputs;
-  const onChange = (e) => {
-    const { name, value } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value,
-    });
-  };
+  // const onChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setInputs({
+  //     ...inputs,
+  //     [name]: value,
+  //   });
+  // };
+  const onChange = useCallback(
+    e => {
+      const { name, value } = e.target;
+      setInputs({
+        ...inputs,
+        [name]: value
+      });
+    },
+    [inputs]
+  );
 
   const [users,  setUsers] = useState([
     {
@@ -43,38 +53,74 @@ function App() {
     }
   ]);
   const nextId = useRef(4);
-  const onCreate = () => {
-    // 나중에 구현 할 배열에 항목 추가하는 로직
-    // ...
+  // const onCreate = () => {
+  //   // 나중에 구현 할 배열에 항목 추가하는 로직
+  //   // ...
+  //   const user = {
+  //     id: nextId.current,
+  //     username,
+  //     email
+  //   };
+  //   // setUsers([...users, user]); //실제 유저가 추가되는 로직
+  //   setUsers(users.concat(user));
+  //   setInputs({
+  //     username: "",
+  //     email: "",
+  //   });
+
+  //   nextId.current += 1;
+    
+  // };
+
+  const onCreate = useCallback(() => {
     const user = {
       id: nextId.current,
       username,
       email
     };
-    // setUsers([...users, user]); //실제 유저가 추가되는 로직
     setUsers(users.concat(user));
+
     setInputs({
-      username: "",
-      email: "",
+      username: '',
+      email: ''
     });
-
     nextId.current += 1;
-    
-  };
+  }, [users, username, email]);
 
-  const onRemove = id => {
-    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
-    // = user.id 가 id 인 것을 제거함
-    setUsers(users.filter(user => user.id !== id));
-  };
-  const onToggle = id => {
 
-    setUsers(
-      users.map(user =>
-        user.id === id ? { ...user, active: !user.active } : user
-      )
-    );
-  };
+  // const onRemove = id => {
+  //   // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+  //   // = user.id 가 id 인 것을 제거함
+  //   setUsers(users.filter(user => user.id !== id));
+  // };
+
+  const onRemove = useCallback(
+    id => {
+      // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+      // = user.id 가 id 인 것을 제거함
+      setUsers(users.filter(user => user.id !== id));
+    },
+    [users]
+  );
+  // const onToggle = id => {
+
+  //   setUsers(
+  //     users.map(user =>
+  //       user.id === id ? { ...user, active: !user.active } : user
+  //     )
+  //   );
+  // };
+
+  const onToggle = useCallback(
+    id => {
+      setUsers(
+        users.map(user =>
+          user.id === id ? { ...user, active: !user.active } : user
+        )
+      );
+    },
+    [users]
+  );
   const count = useMemo(() => countActiveUsers(users), [users]);
   
   return (
