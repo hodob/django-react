5b98ab71999a9fc2a62bb5590b5d8a51ef282c83
지정호 <hodob76@gmail.com>
2023-06-02 15:42:30 +0900

    이제 알같음

--- react_study/react_frontend/src/App.js
+++ react_study/react_frontend/src/App.js
@@ -1,21 +1,65 @@
-import "./App.css";
-
-import Counter from './components/Counter2';
-import InputSample from "./components/InputSample";
+import React, { useState,useRef } from "react";
 import UserList from "./components/UserList";
-
-
-
-
+import CreateUser from "./components/CreateUser";
 
 function App() {
+  const [input, setInputs] = useState({
+    username: "",
+    email: "",
+  });
+  const { username, email } = input;
+  const onChange = (e) => {
+    setInputs({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+    
+  };
 
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: "velopert",
+      email: "public.velopert@gmail.com",
+    },
+    {
+      id: 2,
+      username: "tester",
+      email: "tester@example.com",
+    },
+    {
+      id: 3,
+      username: "liz",
+      email: "liz@example.com",
+    },
+  ]);
+  const nextId = useRef(4);
+  const onCreate = () => {
+    const user = {
+      id : nextId.current,
+      username,
+      email
+    };
+    setUsers([...users, user]);
+
+    setInputs({
+      username: "",
+      email: "",
+    });
+    nextId.current += 1;
+
+  };
 
   return (
     <div>
-        <UserList/>
+      <CreateUser
+        onCreate={onCreate}
+        username={username}
+        onChange={onChange}
+        email={email}
+      />
+      <UserList users={users} />
     </div>
   );
 }
 export default App;
-
