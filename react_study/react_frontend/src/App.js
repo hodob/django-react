adfe09fab46ba75f2bbf4fbe733e1de394eb8c9a
지정호 <hodob76@gmail.com>
2023-06-02 12:29:16 +0900

    (no branch): Auto stash before checking out "HEAD"

--- react_study/react_frontend/src/App.js
+++ react_study/react_frontend/src/App.js
@@ -1,26 +1,49 @@
 
-import React, { useRef, useState, useMemo, useCallback } from "react";
+import React, { useRef, useState, useMemo, useCallback, useReducer } from "react";
 import UserList from "./components/UserList";
 import CreateUser from "./components/CreateUser";
 
-function App() {
   function countActiveUsers(users) {
     console.log('활성 사용자 수를 세는중...');
     return users.filter(user => user.active).length;
   }
+  const initialState = {
+    inputs : {
+      username: '',
+      email : ''
+    },
+    users: [
+      {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
+      active: true
+      },
+      {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com',
+      active: false
+      },
+      {
+        id: 3,
+        username: 'liz',
+        email: 'liz@example.com',
+        active: false
+      }
+    ]
+  };
+  console.log(typeof initialState)
+function App() {
   
+
+  
+
   const [inputs, setInputs] = useState({
     username: '',
     email: ''
   });
   const { username, email } = inputs;
-  // const onChange = (e) => {
-  //   const { name, value } = e.target;
-  //   setInputs({
-  //     ...inputs,
-  //     [name]: value,
-  //   });
-  // };
   const onChange = useCallback(
     e => {
       const { name, value } = e.target;
--- react_study/react_frontend/src/components/Counter.js
+++ react_study/react_frontend/src/components/Counter.js
@@ -1,23 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState,useReducer } from 'react';
+
+
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT' :
+      return state - 1;
+    default:
+      throw new Error('Unhandled action');
+  }
+  
+}
+
+
 
 function Counter() {
-  const [number, setNumber2] = useState(0);
-//   배열 비구조화 할당 통해서 위 코드로 가능
-// const numberState = useState(0);
-// const number = numberState[0];
-// const setNumber = numberState[1];
+  const [number, setNumber] = useState(0);
+  const [number2,dispatch] = useReducer(reducer,0);
+
 
   const onIncrease = () => {
-    setNumber2(number + 1);
+    setNumber(number + 1);
   }
 
   const onDecrease = () => {
-    setNumber2(number - 1);
+    setNumber(number - 1);
+  }
+  const onIncrease2 = () => {
+    dispatch({type:'INCREMENT'})
+  };
+  const onDecrease2 = () => {
+    dispatch({type:'DECREMENT'})
   }
 
   return (
     <div>
-      <h1>{number}</h1>
+      <h1>{number2}</h1>
       <button onClick={onIncrease}>+1</button>
       <button onClick={onDecrease}>-1</button>
     </div>
