0740126157eef96b58bc8d430cf05f60cce93c71
지정호 <hodob76@gmail.com>
2023-06-02 16:29:32 +0900

    디자인 todo

--- to_do/src/App.js
+++ to_do/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { createGlobalStyle } from 'styled-components';
 import TodoTemplate from './components/TodoTemplate';
 import TodoHead from './components/TodoHead';
+import TodoList from './components/TodoList';
+import TodoCreate from './components/TodoCreate';
+
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -13,7 +16,11 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <TodoTemplate> <TodoHead /></TodoTemplate>
+      <TodoTemplate>
+        <TodoHead />
+        <TodoList />
+        <TodoCreate />
+      </TodoTemplate>
     </>
   );
 }
