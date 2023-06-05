670ae9e00d61998382b8926b2c1b7cbc39ec701e
지정호 <hodob76@gmail.com>
2023-06-05 17:11:48 +0900

    redux thunk 사용

--- learn-redux-middleware/src/App.js
+++ learn-redux-middleware/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import PostListContainer from './containers/PostListContainer';
+import { Route ,Routes} from 'react-router-dom';
+import PostListPage from './pages/PostListPage';
+import PostPage from './pages/PostPage';
 
 function App() {
-  return <PostListContainer />;
+  return (
+    <Routes>
+      <Route path="/" element={<PostListPage/>}/>
+      <Route path="/:id" element={<PostPage/>} />
+    </Routes>
+  );
 }
 
 export default App;
